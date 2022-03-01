fd019c3f81cb03d9bfff7e685c4884fdd956db03
Phigu77 <phi.gu@outlook.com>
2022-03-01 09:22:59 +0100

    changes

---
+++ .vscode/avr.properties.mk
@@ -0,0 +1,4 @@
+AVR.source.compiler = c:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\bin\avr-gcc.exe
+AVR.source.libraries = 
+AVR.device.type = 
+AVR.device.frequency = 
--- libs/capacitive.c
+++ libs/capacitive.c
@@ -16,10 +16,10 @@ volatile unsigned long timer1_ovf_count = 0;
 
 void capacitive_init() 
 {
-	CAP_PORT_DIR |=	(1<<CAP_OUT_PIN) |					// Set CAP_OUT_PIN as output
-					(1<<CAP_IN_PIN);					// Set CAP_IN_PIN as output
-	CAP_PORT &=		~(1<<CAP_OUT_PIN) &					// OUT_PIN low
-					~(1<<CAP_IN_PIN);					// IN_PIN low
+	CAP_PORT_DIR |=	(1<<CAP_OUT_PIN) |						// Set CAP_OUT_PIN as output
+					(1<<CAP_IN_PIN);						// Set CAP_IN_PIN as output
+	CAP_PORT &=		~(1<<CAP_OUT_PIN) &						// OUT_PIN low
+					~(1<<CAP_IN_PIN);						// IN_PIN low
 }
 
 uint16_t get_capacitance(void)
@@ -27,25 +27,24 @@ uint16_t get_capacitance(void)
 		// Charge capacitance under test CAP_OUT_PIN and CAP_IN_PIN
 		// Rising high edge on CAP_OUT_PIN
 		
-		CAP_PORT_DIR &= ~(1<<CAP_IN_PIN);				// Set pin CAP_IN_PIN as input
-		ADMUX = (1<<MUX1);								// Enable ADC on CAP_IN_PIN
-		CAP_PORT |= (1<<CAP_OUT_PIN);					// CAP_OUT_PIN high to charge cap 
+		CAP_PORT_DIR &= ~(1<<CAP_IN_PIN);					// Set pin CAP_IN_PIN as input
+		ADMUX = (1<<MUX1);									// Enable ADC on CAP_IN_PIN
+		CAP_PORT |= (1<<CAP_OUT_PIN);						// CAP_OUT_PIN high to charge cap 
 		
-		ADCSRA =	(1<<ADEN) |							// Enable ADC
-					(1<<ADPS2) | (1<<ADPS1) |			// Set prescaler to 64 -> f_ADC=125kHz
-					(1<<ADSC); 							// Start AD-conversion 
-		while (ADCSRA & (1<<ADSC));						// Wait until conversion done
+		ADCSRA =	(1<<ADEN) |								// Enable ADC
+					(1<<ADPS2) | (1<<ADPS1) |				// Set prescaler to 64 -> f_ADC=125kHz
+					(1<<ADSC); 								// Start AD-conversion 
+		while (ADCSRA & (1<<ADSC));							// Wait until conversion done
 		int low  = ADCL;
 		int high = ADCH;
 		int val = (high << 8) | low;
+		ADCSRA &= ~(1<<ADEN);        						// disable ADC
 
 		capacitive_init();		
 	
 	if (val < 800)
 	{
-		//Low value capacitor
-		//short capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
-		//return capacitance;
+		// Low value capacitor
 		return (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
 	}
 	else
@@ -60,28 +59,29 @@ uint16_t get_capacitance(void)
 		timer1_ovf_count = 0;								// Clear timer value
 		
 		TIMSK = (1<<TOIE1);									// enable interrupt on overflow 
-		//sei();												// enable global interrupts
+		//sei();											// enable global interrupts
 
 		ADMUX =  (1<<MUX0);									// AD-MUX on CAP_OUT_PIN
 
 		// Start charging the capacitor trough the internal pull-up resistor 
 		CAP_PORT_DIR  &=  ~(1<<CAP_OUT_PIN);				// Set OUT_PIN as input
 		CAP_PORT |= (1<<CAP_OUT_PIN);						// activate IN_PIN pull-up resistor -> starts charging cap through pullup resistor 
-		TCCR1 = (1<<CS11) | (1<<CS10);						// Prescaler 4 - 2MHz - start timer2
+		TCCR1 = (1<<CS11) | (1<<CS10);						// Prescaler 4 - 2MHz - start timer1
 		
 		while ( !((CAP_PORT_PIN) & (1 << (CAP_OUT_PIN))) && // Wait until PIN_OUT goes high at around 2,5V
 				(timer1_ovf_count < MAX_TIMER1_OVF) );		// or timeout
-		TCCR1 &= ~( (1<<CS11) | (1<<CS10) ) ;				// stop timer2
+		//TCCR1 &= ~( (1<<CS11) | (1<<CS10) ) ;				// stop timer2
+		GTCCR |= (1<<PSR1);									// stop timer2
 		CAP_PORT &= ~(1<<CAP_OUT_PIN);						// stop charging by disabeling the pullup resistor
-		// read value at OUT_PIN
-		ADCSRA =	(1<<ADEN) |								// Enable ADC
+		
+		ADCSRA =	(1<<ADEN) |								// Enable ADC, read value at OUT_PIN
 					(1<<ADPS2) | (1<<ADPS1) |				// Set prescaler 
 					(1<<ADSC); 								// Start AD-conversion 
 		while(ADCSRA & (1<<ADSC));							// Wait until conversion done
 		int low =	ADCL;
 		int high =	ADCH;
 		int val = (high << 8) | low;
-
+		ADCSRA &= ~(1<<ADEN);        						// disable ADC
 		capacitive_init();
 
 		float t_us = ((float)timer1_ovf_count + (float)TCNT1/256) * 128;
